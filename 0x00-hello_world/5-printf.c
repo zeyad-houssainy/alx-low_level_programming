855bedff73b93dc65cf3aac9a5fb80448b641b67
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2022-12-08 18:10:29 -0800

    messagfe

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,10 @@
 
 /**
  *
- *  * main - Entry point
+ * main - Entry point
  *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
