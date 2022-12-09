27e652658065ca813091d1d74a64ef8726c978d6
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2022-12-08 18:41:38 -0800

    message

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+nclude <stdio.h>
+/**
+ * main - Write a C program that prints the size of various types on the computer
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
