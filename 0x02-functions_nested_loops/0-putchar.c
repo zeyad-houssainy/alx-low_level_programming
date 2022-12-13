5a8eca84e2865fc4f98f364ef8e2cab374e4d71a
Zeyad Al-Houssainy <zeyad-houssainy@hotmail.com>
2022-12-14 00:19:54 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 
 /**
@@ -19,23 +18,3 @@ int main(void)
 	_putchar(10);
 	return (0);
 }
-=======
-#include "main.h"
-/**
- * main - prints _putchar, fllowd by a new linees
- * Return: Alwys 0 (Success)
- */
-int main(void)
-{
-	char abe[] = "_putchar";
-
-	int c;
-
-	for (c = 0; c < 8; c++)
-	{
-		_putchar(abe[c]);
-	}
-	_putchar('\n');
-	return (0);
-}
->>>>>>> ed80f07b4b8669b2f7e1583b5d737a8e969e6c26
