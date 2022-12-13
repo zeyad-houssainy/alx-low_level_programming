ed80f07b4b8669b2f7e1583b5d737a8e969e6c26
Zeyad Al-Houssainy <zeyad-houssainy@hotmail.com>
2022-12-14 00:10:05 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,18 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * main - prints a word
- *
- * Description: just prints a statement
- * Return: Always(0) Success
+ * main - prints _putchar, fllowd by a new linees
+ * Return: Alwys 0 (Success)
  */
-
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+	char abe[] = "_putchar";
 
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(abe[c]);
+	}
+	_putchar('\n');
 	return (0);
 }
