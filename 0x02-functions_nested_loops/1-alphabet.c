395f58e60b3b9f86b06410570aeb9aae9ca98dbe
Zeyad Al-Houssainy <zeyad-houssainy@hotmail.com>
2022-12-13 23:46:43 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,15 @@
 #include "main.h"
-
 /**
- * print_alphabet - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-
+*print_alphabet - function that print alphabet
+*it prints in lowercase
+*Return: no return
+*/
 void print_alphabet(void)
 {
-	char alp = 'a';
-
-	while (alp <= 'z')
-	{
-		_putchar (alp);
-		alp++;
-
-	}
-	_putchar ('\n');
-
+int letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar (letter);
+}
+_putchar (10);
 }
