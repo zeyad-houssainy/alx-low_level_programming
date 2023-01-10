f221b749c8ac8d6c6cd632fce33482c6baecefe1
Zeyad Al-Houssainy <zeyad-houssainy@hotmail.com>
2023-01-11 00:14:45 +0200

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,17 +15,14 @@ char *create_array(unsigned int size, char c)
     unsigned int index;
 
     if (size == 0)
-    {
         return (NULL);
-    }
     array = malloc(sizeof(char)*size);
+    
     if (array == NULL)
-    {
         return(NULL);
-    }
+
     for (index=0 ; index<size ; index++)
-    {
         c = array[index];
-    }
+        
     return(array);
 }
