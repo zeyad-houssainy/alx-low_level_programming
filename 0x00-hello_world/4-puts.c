0064065ddad84381e6bf65d03f9730adef1e42d6
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-05-09 00:56:20 +0300

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+*Write a C program that prints exactly
+*"Programming is like building a multilingual puzzle,
+*followed by a new line.
+*
+*return - value(0)
+*
+*/
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle,");
+}
