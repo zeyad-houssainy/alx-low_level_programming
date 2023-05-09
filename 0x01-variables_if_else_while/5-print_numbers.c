c7a4aa5c4f55d09828815bc208845bb6d54f7fdb
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-05-10 01:48:27 +0300

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print all the letters except q and e in lower case
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = '0'; letter <= '9'; ++letter)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
