689bf9c9b0466838b907827445ceee69891ca3d5
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-05-10 01:32:30 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,11 +4,10 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	char letter;
-	for (letter = 'a'; letter<='z'; ++letter)
+	for (letter = 'a'; letter <= 'z'; ++letter)
 	{
 		putchar(letter);
 	}
