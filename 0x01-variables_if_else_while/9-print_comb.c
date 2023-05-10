4c8060271aece2274dce9e175e792fe645d1d75b
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-05-10 20:29:06 +0300

    1

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = '0'; letter <= '9'; ++letter)
+	{
+		putchar(letter);
+		if (letter < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
