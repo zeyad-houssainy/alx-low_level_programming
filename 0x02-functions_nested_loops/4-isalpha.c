df13fd8f31d39de4aa67b1396ca70fc775f7e4a3
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-05-12 02:49:06 +0300

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_islower - controls if a character is in lowercase
+*@c: character to be verified
+*Return: return 0 or 1
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
