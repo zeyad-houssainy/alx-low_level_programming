eb8c219256780715f54c9df71d946974e65bc676
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-05-12 14:53:43 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*_islower - controls if a character is in lowercase
+*function that checks for alphabetic character
 *@c: character to be verified
 *Return: return 0 or 1
 */
