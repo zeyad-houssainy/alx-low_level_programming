23b810a9faaeb874add619c314ccbfd9074f93a0
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-05-12 03:29:21 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
-*_islower - controls if a character is in lowercase
+*_isalpha - controls if a character is alphabetical
 *@c: character to be verified
 *Return: return 0 or 1
 */
-
 int _isalpha(int c)
 {
-	if (c >= 'A' && c <= 'z')
-		return (1);
-	else
-		return (0);
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+return (1);
+
+return (0);
 }
