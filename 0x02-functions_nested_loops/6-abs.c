28c06c05219c41318036b21d873348c2aa044ea9
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-05-13 16:50:31 +0300

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+ int _abs(int i)
+ {
+	if (i > 0)
+	{
+		_putchar(i)
+	}
+	else if (i < 0)
+	{
+		_putchar(-i)
+	}
+	else
+	{
+		_putchar(0)
+	}
+ }
