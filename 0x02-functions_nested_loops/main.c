6c53691c8a0a54e3290c5f7abdf660a86398f09e
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-05-14 23:47:22 +0300

    101

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,60 +1,21 @@
-
 #include <stdio.h>
-#include "main.h"
-int _putchar(char c);
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
+/**
+ * main - prints the sum of all the multiples of 3 or 5 below 1024
+ * @void: void
+ * Return: 0 Success
+ */
 int main(void)
 {
-	int y, x;
-	int temp;
-	int n = 0;
-	if (n <= 15 && n >= 0)
-	{
-		for (x = 0; x <= n; x++)
-		{
+	int n;
+	int sum = 0;
 
-			_putchar('0');
-			for (y = 1; y <= n; y++)
-			{
-				temp = x * y;
-				if (temp < 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(temp + '0');
-				}
-				else if (temp < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((temp / 10) + '0');
-					_putchar((temp % 10) + '0');
-				}
-				else if (temp >= 100 && temp < 1000)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((temp / 100) + '0');
-					_putchar(((temp / 10) % 10) + '0');
-					_putchar((temp % 10) + '0');
-				}
-			}
-			_putchar (10);
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			sum += n;
 		}
 	}
+	printf("%d\n", sum);
+	return (0);
 }
-
-
-//##############################################
-//##############################################
-//##############################################
-//testing file
-// Enter your code below
-
