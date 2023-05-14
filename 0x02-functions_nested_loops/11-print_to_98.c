9e8667590176036a890581ec78166a00ac902d1e
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-05-14 20:52:22 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n < 98; n++)
+		for (; n <= 98; n++)
 		{
 			if (n != 98)
 			{
@@ -25,7 +25,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (; n > 98; n--)
+		for (; n >= 98; n--)
 		{
 			if (n != 98)
 			{
