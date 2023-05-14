4879843709bb569ec6033f667afdae059af65332
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-05-14 20:26:08 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,25 +9,22 @@
 
 void print_to_98(int n)
 {
-int i, j;
-if (n <= 98)
-{
-for (i = n; i <= 98; i++)
-{
-if (i != 98)
-printf("%d, ", i);
-else if (i == 98)
-printf("%d\n", i);
-}
-}
-else if (n >= 98)
-{
-for (j = n; j >= 98; j--)
-{
-if (j != 98)
-printf("%d, ", j);
-else if (j == 98)
-printf("%d\n", j);
-}
-}
+	if (n > 98)
+	{
+		for (n + 1; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n < 98)
+	{
+		for (n + 1; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n == 98)
+	{
+		printf("%d\n", n);
+	}
 }
