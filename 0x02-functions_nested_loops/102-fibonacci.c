4f598f0ac9e3dc81994cc9b4c2366ef375dec1d7
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-05-15 00:26:14 +0300

    5

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main -  prints the first 50 Fibonacci numbers
+* @void: void
+* Return: 0 Success
+*/
+
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+	int sum = 0;
+	int count = 0;
+
+	for (; count < 50; count++)
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+	}
+	printf("%d\n", sum);
+}
