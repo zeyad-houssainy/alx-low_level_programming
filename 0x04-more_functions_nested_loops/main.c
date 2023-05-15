99d14a81b647bdeebfcdbb9a3ea980a64610bc0f
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-05-15 18:54:10 +0300

    1

---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include "main.h"
+int _putchar(char c);
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+int main(void)
+{
+
+}
+
+
+//##############################################
+//##############################################
+//##############################################
+//testing file
+// Enter your code below
+
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+/**
+*File:main.h
+*Author:Pericles Adjovi
+*Desc:header file containing functions declaration
+*/
+
+
+int _putchar(char c);
