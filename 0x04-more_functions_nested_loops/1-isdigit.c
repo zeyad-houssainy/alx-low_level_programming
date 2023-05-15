4b32958eff5a32869081d0e3b8f00ebd66eb914b
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-05-16 00:41:04 +0300

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,12 +2,12 @@
 /**
 *_isupper - function that verifies if a  character is digit
 *@c: tested character
-*Return: returns 1 if it is uppercase , 0 if not
+*Return: returns 1 if it is digit , 0 if not
 */
 
 int _isdigit(int c)
 {
-	if (c = 48 && c <= 57)
+	if ((c = 48) && (c <= 57))
 		return (1);
 	else
 		return (0);
