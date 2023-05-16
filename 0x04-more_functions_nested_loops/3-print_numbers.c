153c493b1a3c84dbd9102989861ea5f3aff8309d
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-05-16 08:22:48 +0300

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_numbers - print number from 0 to 9
+*return: nothing
+*/
+
+
+
+void print_numbers(void);
+{
+	char i;
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+}
