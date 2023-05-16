b29e33fbb114b27fc83f44f23b20e856b2370724
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-05-17 01:31:45 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,43 +13,21 @@ int main(void)
 
 	for (count = 1; count <= 100; count++)
 	{
-		if (count != 100)
+		if ((count % 3 == 0) && (count % 5 == 0))
 		{
-			if ((count % 3 == 0) && (count % 5 == 0))
-			{
-				printf("FizzBuzz ");
-			}
-			else if (count % 5 == 0)
-			{
-				printf("Buzz ");
-			}
-			else if (count % 3 == 0)
-			{
-				printf("Fizz ");
-			}
-			else
-			{
-				printf("%d ", count);
-			}
+			printf("FizzBuzz ");
+		}
+		else if (count % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (count % 3 == 0)
+		{
+			printf("Fizz ");
 		}
 		else
 		{
-			if ((count % 3 == 0) && (count % 5 == 0))
-			{
-				printf("FizzBuzz ");
-			}
-			else if (count % 5 == 0)
-			{
-				printf("Buzz ");
-			}
-			else if (count % 3 == 0)
-			{
-				printf("Fizz ");
-			}
-			else
-			{
-				printf("%d", count);
-			}
+			printf("%d ", count);
 		}
 	}
 	return (0);
