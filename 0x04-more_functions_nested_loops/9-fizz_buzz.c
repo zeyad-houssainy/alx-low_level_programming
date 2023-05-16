9aa3d9fd0794a8f25e82aa154b70f0ef5b0873a0
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-05-17 01:25:47 +0300

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,21 +13,28 @@ int main(void)
 
 	for (count = 1; count <= 100; count++)
 	{
-		if ((count % 3 == 0) && (count % 5 == 0))
+		if (count != 100)
 		{
-			printf("FizzBuzz ");
-		}
-		else if (count % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else if (count % 3 == 0)
-		{
-			printf("Fizz ");
+			if ((count % 3 == 0) && (count % 5 == 0))
+			{
+				printf("FizzBuzz ");
+			}
+			else if (count % 5 == 0)
+			{
+				printf("Buzz ");
+			}
+			else if (count % 3 == 0)
+			{
+				printf("Fizz ");
+			}
+			else
+			{
+				printf("%d ", count);
+			}
 		}
 		else
 		{
-			printf("%d ", count);
+			printf("%d", count);
 		}
 	}
 	return (0);
