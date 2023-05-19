a378039b1b7e7b7ff3ad2916605e227180322896
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-05-19 19:16:05 +0300

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_strlen - returns the length of a string.
+*
+*@s: number 1
+*Return:lnegth
+*/
+
+int _strlen(char *s)
+{
+	int temp, count;
+	for (temp = 0; s[temp] != '\0'; temp++)
+	{
+		count++;
+	}
+}
