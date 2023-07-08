90b1a7016b271ffe3519cacad72df047dc8e2bd3
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-07-08 23:32:02 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,10 @@
 void print_rev(char *s)
 {
 	int count;
+
 	for (count = 0; *(s + count) != '\0'; count++)
 	{
-		_putchar(*(s + count));
 	}
-	_putchar('\n');
 	for (; count != -1; count --)
 	{
 		if (*(s + count) == '\0')
