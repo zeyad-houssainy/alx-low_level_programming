c8ad1095b1bb64d3279f08a9ae2d30516b712320
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-07-12 20:26:03 +0300

    1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *_strcat - concatenates  the string pointed to by @src to
- * the end of the string pointed to by @dest
+ *_strcat - concatenates  the string pointed to by src to
+ * the end of the string pointed to by dest
  *@dest: String that will be appended
  *@src: String to be concatenated upon
  *
