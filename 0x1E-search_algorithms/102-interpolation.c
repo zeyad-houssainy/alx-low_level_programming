1b2db717c9097180a6761ef3607e3b5f5e41dd8d
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-12-13 14:04:43 +0200

    Update 102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,34 +1,40 @@
 #include "search_algos.h"
 
 /**
- * interpolation_search - search the first ocurrency of a value in the array
- * @array: set of numbers
- * @size: size of the array
- * @value: value to search
- * Return: return the first index located otherwise -1
+ *interpolation_search - function that  searches
+ *for a value in a sorted array of integers
+ *@array: pointer to the first element of the array to search in
+ *@size: number of elements in array
+ *@value: the value to search for
+ *Return: first index where value is located
  */
+
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t l = 0, r = size - 1, pos = 0;
+	size_t i, l, r;
 
 	if (array == NULL)
 		return (-1);
-	pos = l + (((double)(r - l) / (array[r] - array[l])) * (value - array[l]));
-	if (pos >= size)
-		printf("Value checked array[%d] is out of range\n", (int)pos);
-	while ((array[r] != array[l]) && (value >= array[l]) && (value <= array[r]))
-	{
-		pos = l + (((double)(r - l) / (array[r] - array[l])) * (value - array[l]));
-		printf("Value checked array[%d] = [%d]\n", (int)pos, array[pos]);
 
-		if (array[pos] < value)
-			l = pos + 1;
-		else if (array[pos] > value)
-			r = pos - 1;
+	for (l = 0, (r = size - 1); r >= l;)
+	{
+		i = l + (((double)(r - l) / (array[r] - array[l])) * (value - array[l]));
+		if (i < size)
+		{
+			printf("Value checked array [%ld] = [%d]\n", i, array[i]);
+		}
 		else
-			return (pos);
+		{
+			printf("Value checked array [%ld] is out of range\n", i);
+			break;
+		}
+
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			r = i - 1;
+		else
+			l = i + 1;
 	}
-	if (array[l] == value)
-		return (l);
 	return (-1);
 }
